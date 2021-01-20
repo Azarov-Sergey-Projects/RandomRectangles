aa0ba9004f134e952291a2d310eaa980cfd71e42
Огурчик <sergeyazarov1006@gmail.com>
2021-01-20 12:33:36 +0200

    Добавьте файлы проекта.

---
+++ Lab3.cpp
@@ -0,0 +1,11 @@
+﻿// Lab3.cpp : Определяет точку входа для приложения.
+//
+#include <Windows.h>
+
+
+
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR lpCmdLine, int nCmdShow)
+{
+
+}
+
